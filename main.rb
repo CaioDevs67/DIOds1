3d8c85ce76d4c54385591ca1c23512d230bba57d
CaioDaniel2 <13979241-CaioDaniel2@users.noreply.replit.com>
2023-04-30 18:16:00 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,8 @@
+print('Digite seu nome:')
+nome = gets.chomp
+print('Digite seu sobrenome:')
+sobrenome = gets.chomp
+print('Digite sua idade:')
+idade = gets.chomp.to_i
+
+puts("Seu nome é #{nome} #{sobrenome} e você possue #{idade} anos")
